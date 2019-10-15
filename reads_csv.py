db9c027eda26ec8dc6118fe6685c6494f0c16e1a
Gamesherd224 <46136202+Gamesherd224@users.noreply.github.com>
2019-10-14 21:43:39 -0500

    Update reads_csv.py

--- reads_csv.py
+++ reads_csv.py
@@ -1,16 +1,18 @@
 #reads_csv.py
 import csv
 import numpy as np
-
-with open('banana_pics.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    '''Turns first image into a numpy array'''
-    for row in csv_reader:
-        if line_count == 0:
-            print (type(row))
-            x = np.array(row)
-            print (x)
-            line_count+=1
-        else:
-             break       
+def read_file(file_name, line_count_p):
+	with open(file_name) as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=',')
+		line_count = 0
+		'''Turns first image into a numpy array'''
+		for row in csv_reader:
+			if line_count == line_count_p:
+				x = np.array(row)
+				break
+			else:
+				if line_count%30==0:
+					print ('new line')
+					
+				line_count+=1
+		return x  
