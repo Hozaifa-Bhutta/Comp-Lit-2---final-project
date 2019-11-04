d3fc8f2f189d3401e559175396aedf51bc109cac
Gamesherd224 <46136202+Gamesherd224@users.noreply.github.com>
2019-11-04 11:06:24 -0600

    Update audio_to_spect.py

--- audio_to_spect.py
+++ audio_to_spect.py
@@ -18,35 +18,23 @@ audios = 'cut_audio_files/'
 pictures = 'raw_pictures/'
 audio_fpath = "/Users/Hozai/Desktop/audios/"
 audio_clips = os.listdir(audio_fpath+audios)
-print("No. of .wav files in audio folder = ",(audio_clips))
-x, sr = librosa.load(audio_fpath+audios+audio_clips[0], sr=44100)
+print("No. of .wav files in audio folder = ",(len(audio_clips)))
+for i in range(len(audio_clips)):
+	x, sr = librosa.load(audio_fpath+audios+audio_clips[0], sr=44100)
 
-print(type(x), type(sr))
-print(x.shape, sr)
-X = librosa.stft(x)
-Xdb = librosa.amplitude_to_db(abs(X))
-plt.figure(figsize=(14, 5))
-librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
-plt.savefig(audio_fpath +pictures+'foo.png')
+	print(type(x), type(sr))
+	print(x.shape, sr)
+	X = librosa.stft(x)
+	Xdb = librosa.amplitude_to_db(abs(X))
+	plt.figure(figsize=(14, 5))
+	librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
+	plt.savefig(audio_fpath +pictures+'foo' + str(i) + '.png')
 
-plt.colorbar()
-plt.show()
+	plt.colorbar()
+	plt.show()
 
-#Opens the image
-	#im = Image.open(audio_fpath + 'foo.png', 'r')
-#Saves it as numpy array
-	#na = np.array(im)
-
-#Saves it in npy 
-	#np.save('test3.npy', na)
-
-
-#Loads it back
-	#d = np.load('test3.npy')
-
-
-im = cv2.imread(audio_fpath + pictures+"foo.png",1)
-print (type(im)) #Print <class 'numpy.ndarray'>
-print (im.size) #prints 2100000
-img = Image.fromarray(im, 'RGB')
-img.show()
+	im = cv2.imread(audio_fpath + pictures+"foo" + str(i) +".png",1)
+	print (type(im)) #Print <class 'numpy.ndarray'>
+	print (im.shape) #prints 2100000
+	img = Image.fromarray(im, 'RGB')
+	img.show()
