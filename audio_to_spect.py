c9694dbf9ee7d196b57d447a429f0b997688c55f
Gamesherd224 <46136202+Gamesherd224@users.noreply.github.com>
2019-11-10 17:37:54 -0600

    Update audio_to_spect.py

--- audio_to_spect.py
+++ audio_to_spect.py
@@ -16,47 +16,49 @@ from pydub import AudioSegment
 	t2 = (i +1)* 60000 
 	newAudio = AudioSegment.from_wav("raw_audio.wav")
 	newAudio = newAudio[t1:t2]
-	newAudio.export('cut_audio_files/cut_audio' + str(i) + '.wav', format="wav") #Exports to a wav file in the current path.'''
-	#print (i)
+	newAudio.export('cut_audio_files/cut_audio' + str(i) + '.wav', format="wav") #Exports to a wav file in the current path.
+	print (i)'''
 np.set_printoptions(threshold=sys.maxsize)
 
 
 audios = 'cut_audio_files/'
 pictures = 'raw_pictures/'
-audio_fpath = "/Users/Hozai/Desktop/audios/"
+audio_fpath = "/home/hozaifa/comp_lit/final-project/"
 audio_clips = os.listdir(audio_fpath+audios)
 print("No. of .wav files in audio folder = ",(len(audio_clips)))
-for i in range(0m,o)
-x, sr = librosa.load(audio_fpath+audios+audio_clips[0], sr=44100)
-print(type(x), type(sr))
-print(x.shape, sr)
-X = librosa.stft(x)
-Xdb = librosa.amplitude_to_db(abs(X))
+for i in range(0,27):
+	x, sr = librosa.load(audio_fpath+audios+audio_clips[i], sr=44100)
+	print(type(x), type(sr))
+	print(x.shape, sr)
+	X = librosa.stft(x)
+	Xdb = librosa.amplitude_to_db(abs(X))
 
 
-plt.figure(figsize=(655, 5))
-librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
-plt.savefig(audio_fpath +'foo' + str(0) + '.png')
+	plt.figure(figsize=(320, 5))
+	librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
+	plt.savefig(audio_fpath +pictures+'foo' + str(i) + '.png')
+	print ('done')
+	print (i)
+	plt.colorbar()
+	#plt.show()
 
-plt.colorbar()
-plt.show()
+	#Opens the image
+		#im = Image.open(audio_fpath + 'foo.png', 'r')
+	#Saves it as numpy array
+		#na = np.array(im)
 
-#Opens the image
-	#im = Image.open(audio_fpath + 'foo.png', 'r')
-#Saves it as numpy array
-	#na = np.array(im)
-
-#Saves it in npy 
-	#np.save('test3.npy', na)
+	#Saves it in npy 
+		#np.save('test3.npy', na)
 
 
-#Loads it back
-	#d = np.load('test3.npy')
+	#Loads it back
+		#d = np.load('test3.npy')
 
 
 im = cv2.imread(audio_fpath +"foo" + str(0) +".png",1)
 print (type(im)) #Print <class 'numpy.ndarray'>
-print (im.size) #prints 2100000
-img = Image.fromarray(im, 'RGB')
-img.show()
+print (im.size) #prints 48000000
+print (im.shape)#(500, 32000, 3)
+#img = Image.fromarray(im, 'RGB')
+#img.show()
 	
