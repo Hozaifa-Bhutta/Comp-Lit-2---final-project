46f51d5cb9ee97a7831f6562e8b71d713d8ee0f3
Gamesherd224 <46136202+Gamesherd224@users.noreply.github.com>
2019-11-10 17:37:02 -0600

    Update frametoimg.py

--- frametoimg.py
+++ frametoimg.py
@@ -6,45 +6,36 @@ import cv2
 import os
 import math
 import numpy as np
-filename = 'audios/test'
 audios = 'cut_audio_files/'
 pictures = 'raw_pictures/'
-audio_fpath = "/Users/Hozai/Desktop/audios/"
+audio_fpath = "/home/hozaifa/comp_lit/final-project/"
 audio_clips = os.listdir(audio_fpath+audios)
 
 bottom_border = 455
+one_second_length = 413.333333
+width = 24800
+height = 446
+depth = 3
+
 def framenumToimg(frame_num):
 	#Determines the second number based on frame number
 	second = (frame_num/100)
+	audio_clip_number = int(second/60)
+	second = second-60*audio_clip_number
 	second_fraction = math.modf(second)[0]
-	if frame_num > 51:
-		raw_im1 = crop_img_func(audio_fpath,pictures,0)
-		raw_im2 = crop_img_func(audio_fpath,pictures,1)
-		#im = raw_im1[:bottom_border, second*len:length[1]]
+	whole_second = int(second)
+	exact_pixel = one_second_length*second
+	minus_50 = exact_pixel-(0.5*one_second_length)
+	plus_50 = exact_pixel+(0.50*one_second_length)
+	im = crop_img_func(audio_fpath,pictures,audio_clip_number)
+	im = im[:, int(minus_50):int(plus_50)]
+	if im.shape[1] == 414:
+		im = im[:, :-1]
 
+	#print (type(im)) #Print <class 'numpy.ndarray'>
+	#print (im.size) #prints 619500
+	#print (im.shape)#(385, 413, 3)
+	return im
+	#img = Image.fromarray(im, 'RGB')
+	#img.show()	
 
-		height = raw_im1.shape[0]
-		width = raw_im1.shape[1]
-
-		main_frame_pix = (second_fraction*width)
-
-		im1 = raw_im1[:, int((main_frame_pix)-(0.5*width)):]
-
-		remaining_frames = 100 - frame_num
-		im2 = raw_im2[:, :int((remaining_frames/100)*width)]
-		print (im2.shape)
-		
-		#deletes borders
-
-
-		img1 = Image.fromarray(im1, 'RGB')
-
-		img2 = Image.fromarray(im2, 'RGB')
-
-		final_im = np.concatenate((im1,im2), axis=1)
-		final_img = Image.fromarray(final_im, 'RGB')
-		final_img.show()
-	
-
-	
-framenumToimg(60)
